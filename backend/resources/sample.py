2cada2dd392cec120f9c6c6012a0cf0666fe59f7
Sebastian Steinmeyer <sebastian.steinmeyer@fh-bielefeld.de>
2020-05-14 16:14:04 +0200

    added sample for db access and comments

--- backend/resources/sample.py
+++ backend/resources/sample.py
@@ -1,22 +1,21 @@
 ## @package backend.resources.register
-#  @author Sebastian Steinmeyer
 #  Handles the register ressources.
 #  See rest api documentation for further information.
-import functools
 from flask import (Blueprint, Response, request)
 from backend.util.db import get_db
 
-bp = Blueprint('sample', __name__, url_prefix='/sample')
+bp = Blueprint('sample', __name__, url_prefix='/sample') # set blueprint name and resource path
 
-## Handles the ressource <base>/register with POST.
+## Handles the ressource <base>/sample with GET and POST.
 @bp.route('',methods=['GET','POST'])
 def register():
-    db = get_db()
+    cursor = get_db().cursor()
     
-    if request.method == 'GET':
-        # do stuff
-        return Response('', status=200)
+    if request.method == 'GET': #handle get request on base_url/sample
+        cursor.execute('SELECT VERSION()') #execute statemant
+        version = cursor.fetchone() #fetch database response | see fetchmany(size=x) and fetchall()
+        return Response(version, status=200)
     
-    if request.method == 'POST':
+    if request.method == 'POST': #handle post request
         # do stuff
         return Response('', status=201)
