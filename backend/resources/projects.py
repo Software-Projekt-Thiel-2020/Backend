cb35e7c16a4137245fe745c9c748c94b2838a483
Maximilian Frömelt <maxifroemelt@gmail.com>
2020-05-21 02:50:37 +0200

    code review fixes

--- backend/resources/projects.py
+++ backend/resources/projects.py
@@ -1,32 +1,52 @@
-from flask import(Blueprint, Response, request, json)
+from flask import Blueprint, request, jsonify
 from backend.util.db import get_db
 
-bp = Blueprint('projects', __name__, url_prefix='/api/projects') #set blueprint name and resource path
+bp = Blueprint('projects', __name__, url_prefix='/api/projects')
 
-## Handles the ressource <base>/api/projects with GET and POST.
-@bp.route('',methods=['GET'])
+
+@bp.route('', methods=['GET'])
 def projects():
-    args = request.args
-    if request.method == 'GET':
-        cursor = get_db().cursor()
-        id = args.get('id')
-        instid = args.get('idinstitution')
-        cursor.execute('use mydb')
-        if id is not None and instid is not None:
-            cursor.execute('select * from project where idProject = %s and fkInstitutionProject = %s', (id, instid))
-        elif id is not None and instid is None:
-            cursor.execute('select * from project where idProject = %s', (id,))
-        elif id is None and instid is not None:
-            cursor.execute('select * from project where idProject = %s and fkInstitutionProject = %s', (instid,))
-        else:
-            cursor.execute('select * from project')
-        
-        json_data = []
-        json_names = ['id', 'name', 'webpage', 'idsmartcontract', 'idinstitution']
-        for result in cursor:
-            json_data.append(dict(zip(json_names,result)))
+    """
+    Handles the resource <base>/api/projects with GET.
 
-        return json_data
-        
+    :return: json data of projects
+    """
+    args = request.args
+    id_project = args.get('id')
+    id_institution = args.get('idinstitution')
+
+    cursor = get_db().cursor()
+    cursor.execute('use mydb')
+
+    if id_project and id_institution:
+        cursor.execute('select * from Project where idProject = %s and fkInstitutionProject = %s', (id_project, id_institution))
+    elif id_project:
+        cursor.execute('select * from Project where idProject = %s', (id_project,))
+    elif id_institution:
+        cursor.execute('select * from Project where fkInstitutionProject = %s', (id_institution,))
     else:
-        abort(400)
+        cursor.execute('select * from Project')
+
+    json_data = []
+    json_names = ['id', 'name', 'webpage', 'idsmartcontract', 'idinstitution']
+    for result in cursor:
+        json_data.append(dict(zip(json_names, result)))
+
+    return jsonify(json_data)
+
+
+@bp.route('', methods=['POST'])
+def projects():
+    """
+    Handles the resource <base>/api/projects with POST.
+
+    :return:
+    """
+
+    # if not logged in:
+    #   return jsonify({'error': 'not logged in'}), 403
+
+    # ToDo: Implement POST /projects
+
+    return "ToDo"
+
