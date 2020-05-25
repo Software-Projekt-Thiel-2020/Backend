9d3041e2d21eb84b6fc42d49143555aca7ce3c91
Mathis B <mbl.85@web.de>
2020-05-26 00:26:50 +0200

    Fixed linter problems except Base variable

--- backend/database/alchemy_decl.py
+++ backend/database/alchemy_decl.py
@@ -1,13 +1,11 @@
 from sqlalchemy import Column, ForeignKey, Integer, VARCHAR, BINARY, TIMESTAMP
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine
 
 Base = declarative_base()
 
 class Project(Base):
     __tablename__ = 'Project'
-    idProject = Column(Integer, primary_key = True)
+    idProject = Column(Integer, primary_key=True)
     nameProject = Column(VARCHAR(45))
     webpageProject = Column(VARCHAR(256))
     fkSmartContractProject = Column(Integer, ForeignKey('SmartContract.idSmartContract'))
@@ -16,13 +14,13 @@ class Project(Base):
 
 class SmartContract(Base):
     __tablename__ = 'SmartContract'
-    idSmartContract = Column(Integer, primary_key = True)
-    blockchainAddrSmartContract(BINARY(20))
+    idSmartContract = Column(Integer, primary_key=True)
+    blockchainAddrSmartContract = Column(BINARY(20))
 
 
 class Milestone(Base):
     __tablename__ = 'Milestone'
-    idMilestone = Column(Integer, primary_key = True)
+    idMilestone = Column(Integer, primary_key=True)
     goalMilestone = Column(Integer)
     requiredVotesMilestone = Column(Integer)
     currentVotesMilestone = Column(Integer)
@@ -32,7 +30,7 @@ class Milestone(Base):
 
 class Institution(Base):
     __tablename__ = 'Institution'
-    idInstitution = Column(Integer, primary_key = True)
+    idInstitution = Column(Integer, primary_key=True)
     nameInstitution = Column(VARCHAR(45))
     webpageInstitution = Column(VARCHAR(256))
     fkSmartContractInstitution = Column(Integer, ForeignKey('SmartContract.idSmartContract'))
@@ -40,17 +38,17 @@ class Institution(Base):
 
 class Voucher(Base):
     __tablename__ = 'Voucher'
-    idVoucher = Column(Integer, primary_key = True)
+    idVoucher = Column(Integer, primary_key=True)
     titleVoucher = Column(VARCHAR(32))
     descriptionVoucher = Column(VARCHAR(1024))
-    usedVoucher = Column(Integer(4))
+    usedVoucher = Column(Integer())
     untilBlockVoucher = Column(Integer)
     fkInstitutionVoucher = Column(Integer, ForeignKey('Institution.idInstitution'))
 
 
 class User(Base):
     __tablename__ = 'User'
-    idUser = Column(Integer, primary_key = True)
+    idUser = Column(Integer, primary_key=True)
     usernameUser = Column(VARCHAR(45))
     firstnameUser = Column(VARCHAR(45))
     lastnameUser = Column(VARCHAR(45))
@@ -61,7 +59,7 @@ class User(Base):
 
 class Donation(Base):
     __tablename__ = 'Donation'
-    idDonation = Column(Integer, primary_key = True)
+    idDonation = Column(Integer, primary_key=True)
     amountDonation = Column(Integer)
     fkMilestoneDonation = Column(Integer, ForeignKey('Milestone.idMilestone'))
     fkUserDonation = Column(Integer, ForeignKey('User.idUser'))
@@ -69,7 +67,7 @@ class Donation(Base):
 
 class Transaction(Base):
     __tablename__ = 'Transaction'
-    idTransaction = Column(Integer, primary_key =  True)
+    idTransaction = Column(Integer, primary_key=True)
     dateTransaction = Column(TIMESTAMP)
     fkSmartContractTransaction = Column(Integer, ForeignKey('SmartContract.idSmartContract'))
     fkUserTransaction = Column(Integer, ForeignKey('User.idUser'))
@@ -77,5 +75,5 @@ class Transaction(Base):
 
 class VoucherUser(Base):
     __tablename__ = 'VoucherUser'
-    fkUserVoucherUser = Column(Integer, ForeignKey('User.idUser'), primary_key = True )
-    fkVucherVoucherUser = Column(Integer, ForeignKey('Voucher.idVoucher'), primary_key = True)
+    fkUserVoucherUser = Column(Integer, ForeignKey('User.idUser'), primary_key=True)
+    fkVucherVoucherUser = Column(Integer, ForeignKey('Voucher.idVoucher'), primary_key=True)
