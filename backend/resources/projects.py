db6698f063335ca8e2ebcd9f1626b7b6daa58ad3
Mathis B <mbl.85@web.de>
2020-05-26 01:14:22 +0200

    Changed default get to query with sqlalchemy

--- backend/resources/projects.py
+++ backend/resources/projects.py
@@ -1,6 +1,9 @@
 """Project Resource."""
 from flask import Blueprint, request, jsonify
-from backend.database.db import get_db
+from sqlalchemy import and_
+from backend.database.db import get_session
+from backend.database.alchemy_decl import Project
+
 
 BP = Blueprint('projects', __name__, url_prefix='/api/projects')
 
@@ -16,25 +19,26 @@ def projects_get():  # noqa
     id_project = args.get('id')
     id_institution = args.get('idinstitution')
 
-    cursor = get_db().cursor()
-    cursor.execute('use mydb')
-
+    session = get_session()
+    result = None
     if id_project and id_institution:
-        cursor.execute(
-            'SELECT * FROM Project WHERE idProject = %s AND fkInstitutionProject = %s',
-            (id_project, id_institution)
-        )
+        result = session.query(Project).filter(and_(Project.idProject==id_project, Project.fkInstitutionProject==id_institution))
     elif id_project:
-        cursor.execute('SELECT * FROM Project WHERE idProject = %s', (id_project,))
+        result = session.query(Project).filter(Project.idProject==id_project)
     elif id_institution:
-        cursor.execute('SELECT * FROM Project WHERE fkInstitutionProject = %s', (id_institution,))
+        result = session.query(Project).filter(Project.fkInstitutionProject==id_institution)
     else:
-        cursor.execute('SELECT * FROM Project')
+        result = session.query(Project)
 
     json_data = []
     json_names = ['id', 'name', 'webpage', 'idsmartcontract', 'idinstitution']
-    for result in cursor:
-        json_data.append(dict(zip(json_names, result)))
+    for r in result:
+        json_data.append(dict(zip(json_names, [
+            r.idProject, 
+            r.nameProject, 
+            r.webpageProject, 
+            r.fkSmartContractProject,
+            r.fkInstitutionProject])))
 
     return jsonify(json_data)
 
