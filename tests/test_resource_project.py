b251ca4026439db32b97730e3568235c9d3acf5d
Maximilian Frömelt <maxifroemelt@gmail.com>
2020-05-30 19:42:59 +0200

    Added more tests, corrected project resource so tests pass

--- tests/test_resource_project.py
+++ tests/test_resource_project.py
@@ -24,3 +24,57 @@ def test_projects_get(client):
     assert res.json[2]["idsmartcontract"] == 2
     assert res.json[2]["name"] == "Softwareprojekt 2020"
     assert res.json[2]["webpage"] == "www.swp.de"
+
+
+def test_projects_get_w_institution(client):
+    """get with id_institution."""
+    res = client.get('/api/projects?idinstitution=3')
+    assert res._status_code == 200
+    assert len(res.json) == 2
+
+    assert res.json[0]["id"] == 2
+    assert res.json[0]["idinstitution"] == 3
+    assert res.json[0]["idsmartcontract"] == 2
+    assert res.json[0]["name"] == "Rangaroek verteidigen"
+    assert res.json[0]["webpage"] == "www.asgard.as"
+
+    assert res.json[1]["id"] == 3
+    assert res.json[1]["idinstitution"] == 3
+    assert res.json[1]["idsmartcontract"] == 2
+    assert res.json[1]["name"] == "Softwareprojekt 2020"
+    assert res.json[1]["webpage"] == "www.swp.de"
+
+
+def test_projects_get_w_non_existant_institution(client):
+    """get with id_institution."""
+    res = client.get('/api/projects?idinstitution=1337')
+    assert res._status_code == 200
+    assert len(res.json) == 0
+
+
+def test_projects_get_w_bad_institution_value(client):
+    """get with id_institution."""
+    res = client.get('/api/projects?idinstitution=abcdefg')
+    assert res._status_code == 400
+    assert len(res.json) == 1
+
+
+def test_projects_get_w_very_big_institution_value(client):
+    """get with id_institution."""
+    res = client.get('/api/projects?idinstitution=4149515568880992958512407863691161151012446232242436899995657329690652811412908146399707048947103794288197886611300789182395151075411775307886874834113963687061181803401509523685375')
+    assert res._status_code == 200
+    assert len(res.json) == 0
+
+
+def test_projects_get_w_bad_param(client):
+    """get with id_institution."""
+    res = client.get('/api/projects?NonExistantParam=3000')
+    assert res._status_code == 200
+    assert len(res.json) == 3
+
+
+def test_projects_get_w_bad_param2(client):
+    """get with id_institution."""
+    res = client.get('/api/projects?nameProject=Computer%20malt%20Bild')
+    assert res._status_code == 200
+    assert len(res.json) == 3
