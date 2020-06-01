4ce3f3dbcf11a0dda071ddb688a71aba2260786c
Lukas H <lukas.h2@hotmail.de>
2020-06-01 18:48:36 +0200

    added basic post handling

--- backend/resources/institutions.py
+++ backend/resources/institutions.py
@@ -2,6 +2,7 @@
 from flask import Blueprint, request, jsonify
 from backend.database.db import DB_SESSION
 from backend.database.model import Institution
+from sqlalchemy import exc
 
 BP = Blueprint('institutions', __name__, url_prefix='/api/institutions')  # set blueprint name and resource path
 
@@ -30,3 +31,40 @@ def institutions_get():
         ])))
 
     return jsonify(json_data)
+
+@BP.route('', methods=['POST'])
+def institutions_post():
+    """
+    Handles POST for resource <base>/api/institutions .
+
+    :return: json response
+    """
+
+    authToken = request.headers.get('authToken')
+    name = request.headers.get('name')
+    webpage = request.headers.get('webpage')
+
+    session = DB_SESSION()
+    results = session.query(Institution)
+
+    #TODO real check
+    #check if logged in
+    if not authToken:
+        return jsonify({'status': 'Nicht eingeloggt'}), 403
+
+    #check if name is already taken
+    for result in results:
+        if(name == result.nameInstitution):
+            return jsonify({'status': 'Name bereits vergeben'}), 403
+
+
+   
+    #TODO uuid refactor + smartcontract_id
+    try:
+        session.add(Institution(idInstitution = 5, nameInstitution = name, webpageInstitution = webpage ,smartcontract_id = "666"))
+        session.commit()
+    except exc.SQLAlchemyError:
+        return jsonify({'status': 'Commit error'}), 400
+    
+
+    return jsonify({'status': 'Institution wurde erstellt'}), 200
