4109e25fd10433c9c918594a667b43cbf841c76b
Maximilian Frömelt <maxifroemelt@gmail.com>
2020-06-01 21:33:38 +0200

    improved code clarity, added type check for parameters

--- backend/resources/donations.py
+++ backend/resources/donations.py
@@ -1,9 +1,9 @@
 """Project Resource."""
 from flask import Blueprint, request, jsonify
+
 from backend.database.db import DB_SESSION
 from backend.database.model import Donation
 from backend.database.model import Milestone
-
 
 BP = Blueprint('donations', __name__, url_prefix='/api/donations')
 
@@ -22,6 +22,22 @@ def donations_get():
     idmilestone_milestone = request.args.get('idmilestone')
     idproject_project = request.args.get('idproject')
 
+    try:
+        if id_donation:
+            int(id_donation)
+        if minamount_donation:
+            int(minamount_donation)
+        if maxamount_donation:
+            int(maxamount_donation)
+        if iduser_user:
+            int(iduser_user)
+        if idmilestone_milestone:
+            int(idmilestone_milestone)
+        if idproject_project:
+            int(idproject_project)
+    except ValueError:
+        return jsonify({"error": "bad argument"}), 400
+
     session = DB_SESSION()
     results = session.query(Donation)
 
@@ -37,14 +53,14 @@ def donations_get():
         results = results.filter(Donation.milestone_id == idmilestone_milestone)
     if idproject_project:
         results = results.join(Donation.milestone).filter(Milestone.project_id == idproject_project)
+
     json_data = []
-    json_names = ['id', 'amount', 'userid', 'milestoneid']
     for result in results:
-        json_data.append(dict(zip(json_names, [
-            result.idDonation,
-            result.amountDonation,
-            result.user_id,
-            result.milestone_id,
-        ])))
+        json_data.append({
+            'id': result.idDonation,
+            'amount': result.amountDonation,
+            'userid': result.user_id,
+            'milestoneid': result.milestone_id,
+        })
 
     return jsonify(json_data)
