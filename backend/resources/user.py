c51f82cd8ec7516ed5c8186e507414348496ba30
Maximilian Frömelt <maxifroemelt@gmail.com>
2020-06-02 01:51:26 +0200

    renamed resource to users, simplified json generation

--- backend/resources/user.py
+++ backend/resources/user.py
@@ -1,46 +1,39 @@
-"""Project Resource."""
+"""User Resource."""
 from flask import Blueprint, request, jsonify
 from backend.database.db import DB_SESSION
 from backend.database.model import User
 
 
-BP = Blueprint('user', __name__, url_prefix='/api/user')
+BP = Blueprint('user', __name__, url_prefix='/api/users')
 
 
 @BP.route('', methods=['GET'])
-def user_get():
+def users_get():
     """
-    Handles GET for resource <base>/api/user .
+    Handles GET for resource <base>/api/users .
 
-    :return: json data of projects
+    :return: json data of users
     """
     args = request.args
     name_user = args.get('username')
-    id_user = args.get('id')
 
     session = DB_SESSION()
     results = session.query(User)
 
-    if name_user and id_user:
-        return jsonify({'error': 'to many Arguments'}), 400
-
     if name_user:
-        results = results.filter(User.usernameUser == name_user)
-    elif id_user:
-        results = results.filter(User.idUser == id_user)
+        results = results.filter(User.usernameUser == name_user)  # ToDo: too narrow?
     else:
         return jsonify({'error': 'missing Argument'}), 400
 
     json_data = []
-    json_names = ['id', 'username', 'firstname', 'lastname', 'email', 'publickey']
     for result in results:
-        json_data.append(dict(zip(json_names, [
-            result.idUser,
-            result.usernameUser,
-            result.firstnameUser,
-            result.lastnameUser,
-            result.emailUser,
-            result.publickeyUser.decode("utf-8").rstrip("\x00"),
-        ])))
+        json_data.append({
+            'id': result.idUser,
+            'username': result.usernameUser,
+            'firstname': result.firstnameUser,
+            'lastname': result.lastnameUser,
+            'email': result.emailUser,
+            'publickey': result.publickeyUser.decode("utf-8").rstrip("\x00"),
+        })
 
     return jsonify(json_data)
