c182266ec7f47cd44ebcae14068fdf48189633e6
Wambo04 <45482648+Wambo04@users.noreply.github.com>
2020-06-02 11:57:50 +0200

    RESTAPI: Userinfo (#34)
    * User API
    
    * Fehler bei falscher anzahl an Argumenten hinzugefügt
    
    * Fehler bei falscher anzahl an Argumenten hinzugefügt
    
    * renamed resource to users, simplified json generation
    
    * Added tests for users
    
    Co-authored-by: Maximilian Frömelt <maxifroemelt@gmail.com>

--- backend/resources/__init__.py
+++ backend/resources/__init__.py
@@ -2,9 +2,11 @@
 # Add new Resources here:
 from . import projects
 from . import institutions
+from . import user
 
 # and here:
 BLUEPRINTS = [
     projects.BP,
     institutions.BP,
+    user.BP,
 ]
