0536df7cd9de495992d659137b15afda1a44076f
Maximilian Frömelt <maxifroemelt@gmail.com>
2020-06-04 23:55:17 +0200

    Added donattions_post (Code by Wambo04)

--- backend/resources/donations.py
+++ backend/resources/donations.py
@@ -1,5 +1,7 @@
 """Project Resource."""
 from flask import Blueprint, request, jsonify
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm.exc import NoResultFound
 
 from backend.database.db import DB_SESSION
 from backend.database.model import Donation
@@ -55,3 +57,45 @@ def donations_get():
         })
 
     return jsonify(json_data)
+
+
+@BP.route('', methods=['POST'])
+def donations_post():
+    """
+    Handles POST for resource <base>/api/donations .
+    :return: "{'status': 'Spende wurde verbucht'}", 201
+    """
+    auth_token = request.args.get('authToken', default=None)
+    idmilestone = request.args.get('idmilestone', default=None)
+    amount = request.args.get('amount', default=None)
+    ether_account_key = request.args.get('etherAccountKey', default=None)  # ToDo: an web3.py ?
+
+    if auth_token is None:
+        return jsonify({'error': 'Not logged in'}), 403
+
+    if None in [idmilestone, amount, ether_account_key]:
+        return jsonify({'error': 'Missing parameter'}), 403
+
+    session = DB_SESSION()
+    results = session.query(Login)
+
+    if session.query(Milestone).get(idmilestone) is None:
+        return jsonify({'error': 'Milestone not found'}), 400
+
+    try:
+        try:
+            results = results.filter(Login.authToken == auth_token).one()
+        except NoResultFound:
+            return jsonify({'error': 'Not logged in'}), 403
+        donations_inst = Donation(
+            amountDonation=amount,
+            user_id=results.user_id,
+            milestone_id=idmilestone
+        )
+
+        session.add(donations_inst)
+        session.commit()
+    except SQLAlchemyError:
+        return jsonify({'status': 'Database error'}), 400
+
+    return jsonify({'status': 'Spende wurde verbucht'}), 201
