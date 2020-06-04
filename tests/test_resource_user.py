0a5dd64e9fbdb047d918e6913a4e7b602f7e9af7
Maximilian Frömelt <maxifroemelt@gmail.com>
2020-06-04 22:04:27 +0200

    Added tests for id resource

--- tests/test_resource_user.py
+++ tests/test_resource_user.py
@@ -44,3 +44,31 @@ def test_users_get_w_special(client):
     res = client.get('/api/users?username=-,.!#*?')
     assert res._status_code == 200
     assert len(res.json) == 0
+
+
+def test_users_id_get_existant_param(client):
+    """get for users id with existant id."""
+    res = client.get('/api/users/1')
+    assert res._status_code == 200
+    assert len(res.json) == 6
+
+
+def test_users_id_get_nonexistant_param(client):
+    """get for users id with invalid id"""
+    res = client.get('/api/users/1337')
+    assert res._status_code == 404
+    assert len(res.json) == 0
+
+
+def test_users_id_get_bad_value(client):
+    """get for users id with bad value"""
+    res = client.get('/api/users/abcdefg')
+    assert res._status_code == 400
+    assert len(res.json) == 1
+
+
+def test_users_id_get_big_value(client):
+    """get for users id with very big int as id"""
+    res = client.get('/api/users/' + "1" * 200)
+    assert res._status_code == 404
+    assert len(res.json) == 0
