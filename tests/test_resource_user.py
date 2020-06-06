4c39668d532ff4f4553962d3b2f76ad4c2f71ae6
Maximilian Frömelt <maxifroemelt@gmail.com>
2020-06-06 20:57:00 +0200

    did some todos for user resource

--- tests/test_resource_user.py
+++ tests/test_resource_user.py
@@ -16,6 +16,19 @@ def test_users_get(client):
     assert res.json[0]["username"] == "LoetkolbenLudwig"
 
 
+def test_users_get2(client):
+    res = client.get('/api/users?username=kolben')
+    assert res._status_code == 200
+    assert len(res.json) == 1
+
+    assert res.json[0]["email"] == "ll@swp.de"
+    assert res.json[0]["firstname"] == "Ludwig"
+    assert res.json[0]["id"] == 1
+    assert res.json[0]["lastname"] == "Loetkolben"
+    assert res.json[0]["publickey"] == "4242424242"
+    assert res.json[0]["username"] == "LoetkolbenLudwig"
+
+
 def test_users_get_wo_param(client):
     res = client.get('/api/users')
     assert res._status_code == 400
@@ -105,4 +118,33 @@ def test_users_put(client):
     assert res.json["lastname"] == "new_lastname"
     assert res.json["email"] == "a@b.com"
 
-# ToDo: more users_put tests
+
+def test_users_put_invalid_email(client):
+    headers = {"authToken": TOKEN_1, "firstname": "new_firstname", "lastname": "new_lastname", "email": "dennis"}
+    res = client.put('/api/users', headers=headers)
+    assert res._status_code == 400
+    assert res.json["error"] == "email is not a valid email"
+
+    res = client.get('/api/users/6')
+    assert res._status_code == 200
+
+    assert res.json["id"] == 6
+    assert res.json["username"] == "sw2020testuser1.id.blockstack"
+    assert res.json["firstname"] == "testuser1"
+    assert res.json["lastname"] == "sw2020"
+    assert res.json["email"] == "testuser1@example.com"
+
+
+def test_users_put_wo_auth(client):
+    headers = {"firstname": "new_firstname", "lastname": "new_lastname", "email": "dennis"}
+    res = client.put('/api/users', headers=headers)
+    assert res._status_code == 401
+
+    res = client.get('/api/users/6')
+    assert res._status_code == 200
+
+    assert res.json["id"] == 6
+    assert res.json["username"] == "sw2020testuser1.id.blockstack"
+    assert res.json["firstname"] == "testuser1"
+    assert res.json["lastname"] == "sw2020"
+    assert res.json["email"] == "testuser1@example.com"
