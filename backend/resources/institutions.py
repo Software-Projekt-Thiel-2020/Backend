05ce49265b2e131a3bee3504bda9d5f6efcf2961
Lukas H <lukas.h2@hotmail.de>
2020-06-06 23:34:06 +0200

    patch implementation

--- backend/resources/institutions.py
+++ backend/resources/institutions.py
@@ -3,7 +3,7 @@ from flask import Blueprint, request, jsonify
 from sqlalchemy.exc import SQLAlchemyError
 
 from backend.database.db import DB_SESSION
-from backend.database.model import Institution
+from backend.database.model import Institution, Transaction
 from backend.resources.helpers import auth_user
 
 BP = Blueprint('institutions', __name__, url_prefix='/api/institutions')  # set blueprint name and resource path
@@ -20,11 +20,22 @@ def institutions_get():
 
     session = DB_SESSION()
     results = session.query(Institution)
-    if id_institution:
-        results = results.filter(Institution.idInstitution == id_institution)
 
     json_data = []
     json_names = ["id", "name", "webpage"]
+
+    if id_institution:
+        institution = results.filter(Institution.idInstitution == id_institution).first()
+        if not institution:
+            return jsonify({'error': 'Institution does not exist'}), 400
+
+        json_data.append(dict(zip(json_names, [
+            institution.idInstitution,
+            institution.nameInstitution,
+            institution.webpageInstitution,
+        ])))
+        return jsonify(json_data)
+
     for result in results:
         json_data.append(dict(zip(json_names, [
             result.idInstitution,
@@ -50,13 +61,59 @@ def institutions_post(user_inst):  # pylint:disable=unused-argument
     # check if name is already taken
     name_exist = session.query(Institution).filter(Institution.nameInstitution == name).first()
     if name_exist:
-        return jsonify({'status': 'Name bereits vergeben'}), 200
+        return jsonify({'error': 'name already exists'}), 400
 
     # Todo: smartcontract_id
     try:
-        session.add(Institution(nameInstitution=name, webpageInstitution=web, smartcontract_id=666))
+        session.add(Institution(nameInstitution=name, webpageInstitution=web, smartcontract_id=1))
         session.commit()
     except SQLAlchemyError:
-        return jsonify({'status': 'Commit error'}), 400
+        return jsonify({'error': 'Database error!'}), 400
 
-    return jsonify({'status': 'Institution wurde erstellt'}), 200
+    return jsonify({'status': 'Institution wurde erstellt'}), 201
+
+
+@BP.route('', methods=['PATCH'])
+@auth_user
+def institutions_patch(user_inst):  # pylint:disable=unused-argument
+    """
+    Handles PATCH for resource <base>/api/institutions .
+    :return: json response
+    """
+    name = request.headers.get('name')
+    web = request.headers.get('webpage')
+    institution_id = request.headers.get('id')
+
+    if web is None and name is None:
+        return jsonify({'error': 'missing patch argument'}), 400
+
+    session = DB_SESSION()
+
+    # check user permission
+    owner = session.query(Institution)
+    owner = owner.join(Transaction, Institution.smartcontract_id == Transaction.smartcontract_id)
+    owner = owner.filter(Transaction.user_id == user_inst.idUser, Institution.idInstitution == institution_id).first()
+
+    if owner:
+        # check if name is already taken
+        name_exist = session.query(Institution).filter(Institution.nameInstitution == name).first()
+
+        institution = session.query(Institution).get(institution_id)
+        if institution is None:
+            return jsonify({'error': 'Institution does not exist'}), 404
+
+        try:
+            if web is not None:
+                institution.webpageInstitution = web
+            if not name_exist:
+                institution.nameInstitution = name
+            else:
+                return jsonify({'error': 'name already exists'}), 400
+
+            session.commit()
+            return jsonify({'status': 'Institution wurde bearbeitet'}), 201
+
+        except SQLAlchemyError:
+            return jsonify({'error': 'Database error!'}), 400
+
+    return jsonify({'error': 'no permission'}), 404
