2b0770c648f854a4178e099cf91bb34f0b5e05eb
Mathis B <mbl.85@web.de>
2020-06-17 13:37:20 +0200

    Added inserting of uploaded file into institution/project

--- backend/resources/file.py
+++ backend/resources/file.py
@@ -26,41 +26,57 @@ def allowed_file(filename):
 # TODO satisfy linter
 @BP.route('', methods=['POST'])
 def file_upload():
-	"""
+    """
     Handles uploading a file for  .
 
     :return: json data of projects
     """
-	
-	id_inst = request.headers.get('idInstitution')
-	id_proj = request.headers.get('idProject')
 
-	if id_inst is None and id_proj is None:
-		return jsonify({'error':'No project/institution given'})
-	
-	if 'file' not in request.files:
-		return jsonify({'error':'No file given'})
+    id_inst = request.headers.get('idInstitution')
+    id_proj = request.headers.get('idProject')
 
-	file = request.files['file']
-	
-	if file.filename == '':
-		return jsonify({'error':'No file given'})
+    if id_inst is None and id_proj is None:
+        return jsonify({'error':'No project/institution given'})
 
-	if not (file and allowed_file(file.filename)):
-		return jsonify({'error':'File extension not allowed'})
+    if 'file' not in request.files:
+        return jsonify({'error':'No file given'})
 
-	# Generate a new filename until on that isnt already taken is given
-	while True:
-		n_filename = str(uuid.uuid4()) + file.filename.split(".")[1]
+    file = request.files['file']
 
-		if n_filename not in os.listdir(current_app.config['UPLOAD_FOLDER']):
-			break
+    if file.filename == '':
+        return jsonify({'error':'No file given'})
 
-	file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], "."+n_filename))
+    if not (file and allowed_file(file.filename)):
+        return jsonify({'error':'File extension not allowed'})
 
-	# TODO add picture to Institution/Project
+    # Generate a new filename until on that isnt already taken is given
+    while True:
+        n_filename = str(uuid.uuid4()) + "." + file.filename.split(".")[1]
 
-	return jsonify({'status':'ok'}), 201
+        if n_filename not in os.listdir(current_app.config['UPLOAD_FOLDER']):
+            break
+
+    file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], n_filename))
+
+    session = DB_SESSION()
+
+    try:
+        if id_inst is not None:
+            inst = session.query(Institution).filter(Institution.idInstitution == id_inst).one()
+            inst.picPathInstitution = n_filename
+            session.add(inst)
+
+        if id_proj is not None:
+            proj = session.query(Project).filter(Project.idProject == id_proj).one()
+            proj.picPathProject = n_filename
+            session.add(proj)
+
+    except NoResultFound:
+        return jsonify(), 404
+
+    session.commit()
+
+    return jsonify({'status':'ok'}), 201
 
 
 @BP.route('/<filename>', methods=['GET'])
