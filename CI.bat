34489910a2e4796df26df3febb6c2bb49abeef3c
Alizame <maxifroemelt@gmail.com>
2020-06-18 23:25:23 +0200

    tests run quicker (using in memory db now) (#78)

--- CI.bat
+++ CI.bat
@@ -1,8 +1,10 @@
 set FLASK_APP=backend
 set FLASK_ENV=development
+set DB_TESTING=1
 echo "PROSPECTOR:"
 prospector -0 backend
 echo "BANDIT:"
 bandit -r backend
 echo "PYTEST:"
 pytest -v
+set DB_TESTING=
