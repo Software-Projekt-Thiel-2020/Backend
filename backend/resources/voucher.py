9fb5bdcb0b58c9c200c381bac0df7d515b4f71d5
Mathis B <mbl.85@web.de>
2020-06-25 00:29:15 +0200

    Added post of voucher to database

--- backend/resources/voucher.py
+++ backend/resources/voucher.py
@@ -30,11 +30,41 @@ def voucher_get():
     return jsonify(json_data), 200
 
 
-@BP.route('', methods=['POST'])
-def voucher_post():
+@BP.route('/user', methods=['POST'])
+@auth_user
+def voucher_post(user):
     """
     Handles DELETE for resource <base>/api/voucher .
 
     :return: json data of projects
     """
-    return jsonify({'status': 'TODO: create route'})
+    idVoucher = request.headers.get('idVoucher')
+
+    session = DB_SESSION()
+    voucher = session.query(Voucher).filter(Voucher.idVoucher == idVoucher).one_or_none()
+
+    if voucher is None:
+        return jsonify({'error': 'Voucher doesnt exist'})
+
+    balance = -1
+    try:
+        balance = WEB3.eth.getBalance(user.publickeyUser)
+    except InvalidAddress:
+        return jsonify({'error': 'given publickey is not valid'}), 400
+
+    if balance < voucher.priceVoucher:
+        return jsonify({'error': 'not enough balance'})
+
+    association = VoucherUser(usedVoucher=False,
+                              expires_unixtime=datetime(2020, 12, 31))
+    association.voucher = voucher
+    user.append(association)
+    
+    # TODO - do blockchain transaction
+
+    session.add(voucher)
+    session.add(association)
+    session.add(user)
+    session.commit()
+
+    return jsonify({'status': 'voucher bought'})
