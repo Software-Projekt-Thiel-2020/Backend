aa6cd3f57e0a692d4ffc07ee74c07482d87461fc
Lukas H <lukas.h2@hotmail.de>
2020-06-24 23:58:33 +0200

    satisfy linter

--- backend/resources/voucher.py
+++ backend/resources/voucher.py
@@ -1,7 +1,8 @@
 """Voucher Resource."""
 from flask import Blueprint, jsonify, request
+from sqlalchemy.orm.exc import NoResultFound
 from backend.database.db import DB_SESSION
-from backend.database.model import Voucher,VoucherUser
+from backend.database.model import Voucher, VoucherUser
 from backend.resources.helpers import auth_user
 
 BP = Blueprint('voucher', __name__, url_prefix='/api/voucher')
@@ -72,11 +73,12 @@ def voucher_delete_user(user_inst):
     session = DB_SESSION()
     voucher = session.query(VoucherUser)
     try:
-        voucher = voucher.filter(VoucherUser.id_voucher == id_voucher).filter(VoucherUser.id_user == user_inst.idUser).first()
+        voucher = voucher.filter(VoucherUser.id_voucher == id_voucher).filter(
+            VoucherUser.id_user == user_inst.idUser).first()
     except NoResultFound:
-        return jsonify({'error': 'User has no vouchers'}), 404
-   
+        return jsonify({'error': 'No voucher found'}), 404
+
     voucher.usedVoucher = True
     session.commit()
 
-    return jsonify({'status': 'Gutschein wurde eingelöst'+str(voucher.id_user)}), 201
+    return jsonify({'status': 'Gutschein wurde eingelöst'}), 201
