3a88b8133dfa55fcd00e90d50908efb5230ede29
Maximilian Frömelt <maxifroemelt@gmail.com>
2020-06-29 17:26:58 +0200

    Added ether fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,30 @@
 import os
 import tempfile
 import pytest
+from web3.types import RPCEndpoint
+
 from backend import create_app
 from backend.database import db
+from backend.smart_contracts.web3 import WEB3
+
+
+@pytest.fixture
+def client_w_eth():
+    db.init_db()
+
+    db_fd, tmpfile = tempfile.mkstemp()
+    conf = {
+        "DATABASE": tmpfile,
+        "TESTING": True
+    }
+
+    app = create_app(conf)
+
+    with app.test_client() as client:
+        yield client
+
+    os.close(db_fd)
+    os.unlink(app.config['DATABASE'])
 
 
 @pytest.fixture
@@ -17,8 +39,17 @@ def client():
 
     app = create_app(conf)
 
+    WEB3.provider.make_request("evm_snapshot", [])
+    WEB3.eth.sendTransaction({'from': WEB3.eth.accounts[9],
+                              'to': '0x865fefF6a8503405f8a316e53039dc8332a5A60b',  # sw2020testuser1.id.blockstack
+                              'value': 1 * 10**18})
+    WEB3.eth.sendTransaction({'from': WEB3.eth.accounts[9],
+                              'to': '0x7Dca2Ba711f089C608ABe8C6F59Fe7B5F84fced8',  # sw2020testuser2.id.blockstack
+                              'value': 1 * 10**18})
+
     with app.test_client() as client:
         yield client
 
+    WEB3.provider.make_request("evm_revert", [1, ])
     os.close(db_fd)
     os.unlink(app.config['DATABASE'])
