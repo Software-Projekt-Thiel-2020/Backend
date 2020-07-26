f6d96ce9180deeeac658d020cc8fcd3abbd4ebdc
Maximilian Frömelt <maxifroemelt@gmail.com>
2020-07-26 16:29:55 +0200

    fixed sc tests

--- backend/smart_contracts/migrations/2_deploy_contracts.js
+++ backend/smart_contracts/migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 const Institution = artifacts.require("Institution");
 
-module.exports = function(deployer) {
-  deployer.deploy(Institution);
+module.exports = function(deployer, network, accounts) {
+  deployer.deploy(Institution, accounts[1], accounts[0]);
 };
