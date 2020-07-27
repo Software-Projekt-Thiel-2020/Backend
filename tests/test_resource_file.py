431578fc821c54717134ae0afcbf6d9ce5f5992f
Maximilian Kleinke <Maxi.Kleinke@Nagiry.de>
2020-07-27 17:51:08 +0200

    tests for file_post

--- tests/test_resource_file.py
+++ tests/test_resource_file.py
@@ -1,21 +1,18 @@
 from flask import current_app
+import requests
 
 from .test_blockstackauth import TOKEN_1, TOKEN_2, TOKEN_3
 import os
 
-# 88c0bc0a-c673-4cdf-8216-cd4e2c916be2.png
 image1 = "4eb9a451-2be6-4f98-bb62-3d5673d0c120.png"
+#image2 = "88c0bc0a-c673-4cdf-8216-cd4e2c916be2.png"
 
 def test_file_get(client):
     res = client.get('/api/file/{}'.format(image1))
 
-    try:
-        image = open(os.path.join(current_app.config['UPLOAD_FOLDER'], image1), "rb")
-        image_data = image.read()
-    except:
-        print("exception in test_file_get (file operations)")
-    finally:
-        image.close()
+    image = open(os.path.join(current_app.config['UPLOAD_FOLDER'], image1), "rb")
+    image_data = image.read()
+    image.close()
 
     assert res.status_code == 200
     assert res.data == image_data
@@ -30,14 +27,81 @@ def test_file_get_invalid_file(client):
 def test_file_get_path_traversal(client):
     res=client.get('/api/file/../.gitignore')
 
-    try:
-        file = open(os.path.join(current_app.config['ROOT_DIR'], ".gitignore"), "rb")
-        file_data = file.read()
-    except:
-        print("exception in test_file_get_path_traversal")
-    finally:
-        file.close()
+    file = open(os.path.join(current_app.config['ROOT_DIR'], ".gitignore"), "rb")
+    file_data = file.read()
+    file.close()
 
     assert res.data != file_data
     assert res.status_code == 404
+
+
+def test_file_post(client):
+    res = client.get('/api/file/dummy')
+
+    headers = {"authToken": TOKEN_1, "idInstitution": "1", "idProject": "1"}
+    file = open(os.path.join(current_app.config['TEST_UPLOAD_FOLDER'], "cat.png"), "rb")
+    data = {'file': file}
+
+    res=client.post('/api/file', headers=headers, data=data)
+
+    assert res.status_code == 201
+
+    # Open file again because its getting closed
+    file = open(os.path.join(current_app.config['TEST_UPLOAD_FOLDER'], "cat.png"), "rb")
+    file_data = file.read()
+
+    # Cleanup
+    for r, d, f in os.walk(current_app.config['UPLOAD_FOLDER']):
+        for item in f:
+            currFile_path = os.path.join(r, item)
+            currFile = open(currFile_path, "rb")
+            currFile_data = currFile.read()
+
+            # Search the testfile (by comparing data) and delete it
+            if currFile_data == file_data:
+                currFile.close()
+                os.remove(currFile_path)
+            else:
+                currFile.close()
+
+    file.close()
+
+
+def test_file_post_wout_auth(client):
+    res = client.get('/api/file/dummy')
+
+    headers = {"idInstitution": "1", "idProject": "1"}
+    file = open(os.path.join(current_app.config['TEST_UPLOAD_FOLDER'], "cat.png"), "rb")
+    data = {'file': file}
+
+    res = client.post('/api/file', headers=headers, data=data)
+    file.close()
+
+    assert res.status_code == 401
+
+
+def test_file_post_missing_params(client):
+    res = client.get('/api/file/dummy')
+
+    headers = {"authToken": TOKEN_1}
+    file = open(os.path.join(current_app.config['TEST_UPLOAD_FOLDER'], "cat.png"), "rb")
+    data = {'file': file}
+
+    res = client.post('/api/file', headers=headers, data=data)
+    file.close()
+
+    assert res.status_code == 400
+
+
+def test_file_post_wrong_extension(client):
+    res = client.get('/api/file/dummy')
+
+    headers = {"authToken": TOKEN_1, "idInstitution": "1", "idProject": "1"}
+    file = open(os.path.join(current_app.config['TEST_UPLOAD_FOLDER'], "cat.asd"), "rb")
+    data = {'file': file}
+
+    res = client.post('/api/file', headers=headers, data=data)
+    file.close()
+
+    assert res.status_code == 403
 
