e02cdae5dc2481fc8677db0076a8ddde3d509d88
Batschko <lukas.h2@hotmail.de>
2020-08-01 13:49:19 +0200

    added permission check

--- backend/resources/projects.py
+++ backend/resources/projects.py
@@ -268,6 +268,11 @@ def projects_patch(session, user_inst, id):  # pylint:disable=invalid-name,redef
     if description is not None:
         project_inst.descriptionProject = description
 
+    result = session.query(Institution)\
+        .filter(Institution.idInstitution == id_institution).filter(Institution.user == user_inst).one_or_none()
+    if result is None:
+        return jsonify({'error': 'User has no permission to create projects for this institution'}), 400
+
     projects_sc = WEB3.eth.contract(address=project_inst.scAddress, abi=PROJECT_JSON["abi"])
     try:
         milestones_inst: List[Milestone] = []
